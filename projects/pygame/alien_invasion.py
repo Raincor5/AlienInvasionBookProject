1c8bbf6b5c1b80271ca566dc31ea787822dc587f
Raincor <30125900@nescol.ac.uk>
2023-10-19 22:28:40 +0100

    Inititalised the basics

---
+++ projects/pygame/alien_invasion.py
@@ -0,0 +1,33 @@
+import sys
+import pygame as pg
+
+class AlienInvasion:
+    """Overall class to manage game assets and behaviour"""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pg.init()
+
+        self.clock = pg.time.Clock()
+        self.screen = pg.display.set_mode((1200, 800))
+        pg.display.set_caption("Alien Invasion")
+        self.bg_color = (230, 230, 230)
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    sys.exit()
+
+            self.screen.fill(self.bg_color)
+
+            # Make the most recently drawn screen visible.
+            pg.display.flip()
+            self.clock.tick(60)
+
+
+if __name__ == "__main__":
+    ai = AlienInvasion()
+    ai.run_game()

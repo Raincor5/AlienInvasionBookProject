907369d83d7a346f4014c474bf3321dedecd3063
Raincor <30125900@nescol.ac.uk>
2023-10-20 01:32:41 +0100

    Added ship movement to left and right

--- projects/pygame/alien_invasion.py
+++ projects/pygame/alien_invasion.py
@@ -21,18 +21,35 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    sys.exit()
-
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            # Make the most recently drawn screen visible.
-            pg.display.flip()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
             self.clock.tick(60)
 
+    def _check_events(self):
+        # Watch for keyboard and mouse events.
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                sys.exit()
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_RIGHT:
+                    # Move the ship to the right
+                    self.ship.moving_right = True
+                elif event.key == pg.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pg.KEYUP:
+                if event.key == pg.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pg.K_LEFT:
+                    self.ship.moving_left = False
+
+    def _update_screen(self):
+        """Updates images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # Make the most recently drawn screen visible.
+        pg.display.flip()
 
 if __name__ == "__main__":
     ai = AlienInvasion()

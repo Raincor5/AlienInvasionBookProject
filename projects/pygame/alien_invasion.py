527b6164b2e58726c2535e3d542c891779419608
Raincor <30125900@nescol.ac.uk>
2023-10-21 00:58:44 +0100

    Implemented bullet mechanics into the game.

--- projects/pygame/alien_invasion.py
+++ projects/pygame/alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame as pg
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -18,6 +19,7 @@ class AlienInvasion:
         self.settings.screen_height = self.screen.get_rect().height
         pg.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
+        self.bullets = pg.sprite.Group()
         self.bg_color = (230, 230, 230)
 
     def run_game(self):
@@ -25,6 +27,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
             self._update_screen()
             self.clock.tick(60)
 
@@ -46,6 +49,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pg.K_q:
             sys.exit()
+        elif event.key == pg.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_event(self, event):
         if event.key == pg.K_RIGHT:
@@ -53,9 +58,26 @@ class AlienInvasion:
         elif event.key == pg.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Update position of bullets amd get rid of old bullets."""
+        # Update bullet positions.
+        self.bullets.update()
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
     def _update_screen(self):
         """Updates images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            # bullet.draw_bullet()
+            bullet.draw()
         self.ship.blitme()
 
         # Make the most recently drawn screen visible.

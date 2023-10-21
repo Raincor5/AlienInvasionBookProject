e81f526b666f6cea19d7d7c049d4d854b046ab1e
Raincor <30125900@nescol.ac.uk>
2023-10-21 02:16:31 +0100

    Made a fleet of aliens appear on the screen.

--- projects/pygame/alien_invasion.py
+++ projects/pygame/alien_invasion.py
@@ -73,18 +73,24 @@ class AlienInvasion:
         # Create an alien and keep adding aliens until there's no room left.
         # Spacing between aliens is one alien width.
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
 
-        current_x = alien_width
-        while current_x < (self.settings.screen_width - 2 * alien_width):
-            self._create_alien(current_x)
-            current_x += 2 * alien_width
+        current_x, current_y = alien_width, alien_height
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
 
-    def _create_alien(self, x_positon):
+            # Finished a row; reset x value, and increment y value.
+            current_x = alien_width
+            current_y += 2 * alien_height
+
+    def _create_alien(self, x_position, y_position):
         """Create an alien and place it in the row."""
         new_alien = Alien(self)
-        new_alien.x = x_positon
-        new_alien.rect.x = x_positon
+        new_alien.x = x_position
+        new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
         self.aliens.add(new_alien)
 
     def _update_bullets(self):
